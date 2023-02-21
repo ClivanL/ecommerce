17958b107287ed7e317b2eae5d09499a4d84b445
Clivan Lee <clivan93@gmail.com>
2023-02-21 21:24:57 +0800

    added getMapping for search feature using products item name, into itemController.

--- src/main/java/com/example/ecommerce/item/ItemController.java
+++ src/main/java/com/example/ecommerce/item/ItemController.java
@@ -5,22 +5,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path="/api/item")
 public class ItemController {
 
     private final ItemService itemService;
+    private final ItemRepository itemRepository;
 
     @Autowired
-    public ItemController(ItemService itemService){
+    public ItemController(ItemService itemService, ItemRepository itemRepository){
         this.itemService=itemService;
+        this.itemRepository=itemRepository;
     }
 
     @GetMapping
     public List<Item> getItems() {
         return itemService.getItems();
     }
+
+    @GetMapping("search")
+    public @ResponseBody Optional<Item> getItem(@RequestParam("search") String itemName){
+        Optional<Item> item=itemRepository.findByItemName(itemName);
+        if (item!=null){
+            return item;
+        }
+        else{
+            return null;
+        }
+    }
+
     @PostMapping
     public void addItem(@RequestBody Item item){
         itemService.addItem(item);
@@ -34,4 +49,5 @@ public class ItemController {
         itemService.updateItem(itemId, item);
     }
 }
+
 
