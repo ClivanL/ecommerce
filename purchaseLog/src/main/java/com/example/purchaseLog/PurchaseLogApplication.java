e28223e889f421918a766933ab24a3f7b42915e1
Clivan Lee <clivan93@gmail.com>
2023-03-17 22:35:31 +0800

    init cart and purchaseLog

---
+++ purchaseLog/src/main/java/com/example/purchaseLog/PurchaseLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.purchaseLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseLogApplication.class, args);
+	}
+
+}
