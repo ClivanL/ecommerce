a59e007ec8fdd939aad6d3c73fde1262e33aea29
clivan <clivan93@gmail.com>
2023-03-26 14:41:29 +0800

    amended pom.xml to change java version to 11, amended docker-compose file, added depends_on condition, tested and running

--- user/src/main/java/com/example/user/UserController.java
+++ user/src/main/java/com/example/user/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.random.RandomGenerator;
 
 @RestController
 @RequestMapping(path="/api/user")
