925397df90a9956b744e91ac53c16c54a55403e3
clivan <clivan93@gmail.com>
2023-03-28 23:32:22 +0800

    added delete cart by cartId into main controller and cart controller

--- cart/src/main/java/com/example/cart/CartController.java
+++ cart/src/main/java/com/example/cart/CartController.java
@@ -42,4 +42,9 @@ public class CartController {
         cartService.updateCart(cart);
     }
 
+    @DeleteMapping(path="{cartId}")
+    public void deleteCart(@PathVariable("cartId") Long cartId){
+       cartRepository.deleteById(cartId);
+    }
+
 }
