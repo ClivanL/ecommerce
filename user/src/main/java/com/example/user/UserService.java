6cce605a55759a6c8c619b1865296436dee3eb5f
clivan <clivan93@gmail.com>
2023-04-13 19:22:48 +0800

    account creation in user container to encode password, password in database is stored after encoding

--- user/src/main/java/com/example/user/UserService.java
+++ user/src/main/java/com/example/user/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,11 +18,13 @@ import java.util.Optional;
 @Service
 public class UserService{
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder=passwordEncoder;
     }
 
 
@@ -42,9 +45,8 @@ public class UserService{
         if(userOptionalEmail.isPresent()){
             throw new IllegalStateException("Email in use");
         }
-
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
-
     }
 
     public void deleteUser(Long userId) {
