85f5b20d9bf19b7655b247cc70eb09c2ba58c909
clivan <clivan93@gmail.com>
2023-04-15 11:17:38 +0800

    oh cors you pesky cookie monster

--- main/src/main/java/com/example/main/config/WebSecurityConfig.java
+++ main/src/main/java/com/example/main/config/WebSecurityConfig.java
@@ -82,7 +82,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
+        http.cors().and().csrf()
                 .disable()
                 .authorizeRequests()
                 //.antMatchers(HttpMethod.DELETE)
